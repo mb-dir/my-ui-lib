932d6dc3a36bdcb14fbef49c23f17651e82cabb7
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-02-12 22:10:21 +0100

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed } from "vue";
+type BaseButtonProps = {
+  size?: number;
+  color?: string;
+};
+
+const props = withDefaults(defineProps<BaseButtonProps>(), {
+  size: 16,
+  color: "skyblue",
+});
+
+const fontSize = computed(() => `${props.size}px`);
+</script>
+
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style>
+.button {
+  padding: 1rem 2rem;
+  cursor: pointer;
+  border: none;
+  background-color: v-bind(color);
+  font-size: v-bind(fontSize);
+}
+</style>
